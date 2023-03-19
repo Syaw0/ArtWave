31d4c9f57991301f95f6bf31eff8ecc1f89c781e
Siavash <siamhb7@protonmail.com>
2023-03-19 15:11:40 +0330

    work on the base classes of guard and result.

---
+++ src/shared/core/guard.ts
@@ -0,0 +1,43 @@
+import { Result } from "./result";
+
+export type GuardResponse = string;
+export type GuardArgumentCollection = IGuardArgument[];
+export interface IGuardArgument {
+  argument: any;
+  argumentName: string;
+}
+
+export class Guard {
+  public static combine(guardResults: Result<any>[]): Result<GuardResponse> {
+    for (let result of guardResults) {
+      if (result.isFailure) return result;
+    }
+
+    return Result.ok<GuardResponse>();
+  }
+
+  public static againstNullOrUndefined(
+    argument: any,
+    argumentName: string
+  ): Result<GuardResponse> {
+    if (argument == null) {
+      return Result.fail<GuardResponse>(`${argumentName} is null or undefined`);
+    } else {
+      return Result.ok<GuardResponse>();
+    }
+  }
+
+  public static againstNullOrUndefinedBulk(
+    args: GuardArgumentCollection
+  ): Result<GuardResponse> {
+    for (let arg of args) {
+      const result = this.againstNullOrUndefined(
+        arg.argument,
+        arg.argumentName
+      );
+      if (result.isFailure) return result;
+    }
+
+    return Result.ok<GuardResponse>();
+  }
+}
