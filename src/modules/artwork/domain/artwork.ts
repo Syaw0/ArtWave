ed88b45520158e7ac14c80c1ee18674066753e16
Siavash <siamhb7@protonmail.com>
2023-04-03 19:57:53 +0330

    working on the watched list abstraction.

--- src/modules/artwork/domain/artwork.ts
+++ src/modules/artwork/domain/artwork.ts
@@ -1,10 +1,14 @@
 import { AggregateRoot } from "../../../shared/domain/aggregateRoot";
+import { ArtistId } from "../../artist/domain/artistId";
 import { ArtworkDescription } from "./artworkDescription";
 import { ArtworkId } from "./artworkId";
 
 interface ArtworkProps {
   artworkId: ArtworkId;
   description: ArtworkDescription;
+  owner: ArtistId;
+  publishDate: Date;
+  imageSrc: string;
 }
 
 export class Artwork extends AggregateRoot<ArtworkProps> {
@@ -12,6 +16,15 @@ export class Artwork extends AggregateRoot<ArtworkProps> {
     return ArtworkId.create(this._id).getValue();
   }
   get description(): ArtworkDescription {
-    return this.description;
+    return this.props.description;
+  }
+  get owner(): ArtistId {
+    return this.props.owner;
+  }
+  get publishDate(): Date {
+    return this.props.publishDate;
+  }
+  get imageSrc(): string {
+    return this.props.imageSrc;
   }
 }
