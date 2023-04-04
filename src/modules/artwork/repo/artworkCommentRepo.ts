2e716f05de1c213eee62ea7168c9a0e4d01f07c4
Siavash <siamhb7@protonmail.com>
2023-04-05 02:43:53 +0330

    adding extra methods on the comment and vote repos.

--- src/modules/artwork/repo/artworkCommentRepo.ts
+++ src/modules/artwork/repo/artworkCommentRepo.ts
@@ -1,10 +1,13 @@
 import { OrmType } from "../../../shared/infra/db/orm/createModel";
 import { ArtworkId } from "../domain/artworkId";
 import { Comment } from "../domain/comment";
+import { CommentId } from "../domain/commentId";
 import { ArtworkCommentMapper } from "../mapper/artworkCommentMapper";
 
 export interface ArtworkCommentRepoProps {
   findArtworkComments(artworkId: ArtworkId | string): Promise<Comment[]>;
+  save(comment: Comment): Promise<void>;
+  exist(commentId: CommentId | string): Promise<boolean>;
 }
 
 export class ArtworkCommentRepo implements ArtworkCommentRepoProps {
@@ -21,5 +24,25 @@ export class ArtworkCommentRepo implements ArtworkCommentRepoProps {
     });
     if (comments.length == 0) return [];
     return comments.map((cm: any) => ArtworkCommentMapper.toDomain(cm));
+  }
+
+  async exist(commentId: string | CommentId): Promise<boolean> {
+    const artworkCommentModel = this.model.artworkCommentModel;
+    const id =
+      typeof commentId === "string" ? commentId : commentId.id.toString();
+    const comment = await artworkCommentModel.findOne({
+      where: {
+        artwork_comment_id: id,
+      },
+    });
+    return !!comment == true && comment.length !== 0;
+  }
+
+  async save(comment: Comment): Promise<void> {
+    const artworkCommentModel = this.model.artworkCommentModel;
+    const isExist = await this.exist(comment.commentId);
+    if (!isExist) {
+      artworkCommentModel.create(ArtworkCommentMapper.toPersistence(comment));
+    }
   }
 }
--- src/modules/artwork/repo/artworkVoteRepo.ts
+++ src/modules/artwork/repo/artworkVoteRepo.ts
@@ -1,9 +1,12 @@
 import { OrmType } from "../../../shared/infra/db/orm/createModel";
 import { ArtworkId } from "../domain/artworkId";
 import { ArtworkVote } from "../domain/artworkVote";
+import { ArtworkVoteMapper } from "../mapper/artworkVoteMapper";
 
 export interface ArtworkVoteRepoProps {
   getArtworkVotes(artworkId: ArtworkId | string): Promise<ArtworkVote[]>;
+  save(artworkVote: ArtworkVote): Promise<void>;
+  exist(artworkVoteId: string): Promise<boolean>;
 }
 
 export class ArtworkVoteRepo implements ArtworkVoteRepoProps {
@@ -18,6 +21,28 @@ export class ArtworkVoteRepo implements ArtworkVoteRepoProps {
         artwork_vote_artwork_id: id,
       },
     });
-    return votes.length == 0 || votes == null ? [] : votes;
+    return votes.length == 0 || votes == null
+      ? []
+      : votes.map((v: any) => ArtworkVoteMapper.toDomain(v));
+  }
+
+  async exist(artworkVoteId: string): Promise<boolean> {
+    const artworkVoteModel = this.model.artworkVoteModel;
+    const vote = await artworkVoteModel.findOne({
+      where: {
+        artwork_vote_id: artworkVoteId,
+      },
+    });
+    return !!vote == true && vote.length !== 0;
+  }
+
+  async save(artworkVote: ArtworkVote): Promise<void> {
+    const artworkVoteModel = this.model.artworkVoteModel;
+    const isExist = await this.exist(artworkVote.id.toString());
+    if (!isExist) {
+      await artworkVoteModel.create(
+        ArtworkVoteMapper.toPersistence(artworkVote)
+      );
+    }
   }
 }
